7b2384ae06a8333f084c9729325be083a12063b2
Lucaponz91 <lucapo1991@msn.com>
2022-09-22 16:57:22 +0200

    add css

--- faq.php
+++ faq.php
@@ -5,14 +5,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <title>Faq</title>
 </head>
 
 <body>
-    <?php $faqs = [
-        [
-            "question" => "<h2>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h2>",
-            "answer" => "<p>La recente decisione della Corte di giustizia dell'Unione europea 
+    <!-- HEADER -->
+    <header>
+        <div class="row">
+            <ul class="header_ul">
+                <?php
+                $lists = ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologie', 'Domande frequenti'];
+                foreach ($lists as $list) {
+                    echo "<li class=''>" . "<a href='#' class=''>" . $list . "</a>" . "</li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </header>
+
+    <!-- MAIN -->
+    <main>
+        <?php $faqs = [
+            [
+                "question" => "<h2>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h2>",
+                "answer" => "<p>La recente decisione della Corte di giustizia dell'Unione europea 
             ha profonde conseguenze per i motori di ricerca in Europa. 
             La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di 
             ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il 
@@ -49,11 +69,11 @@
             maggior parte dei nomi, non soltanto le pagine che sono state 
             soggette a una rimozione.</p>
             "
-        ],
-        [
-            "question" => "<h2>Come fa Google a proteggere 
+            ],
+            [
+                "question" => "<h2>Come fa Google a proteggere 
             la mia privacy e a tenere le mie informazioni al sicuro?</h2>",
-            "answer" => "<p> Sappiamo che la sicurezza e la privacy sono importanti per te 
+                "answer" => "<p> Sappiamo che la sicurezza e la privacy sono importanti per te 
             e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti 
             che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</p>
             <p> Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua 
@@ -68,10 +88,10 @@
             te e la tua famiglia online, nel Centro Google per la sicurezza online. </p>
             <p> Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue 
             informazioni personali, lasciando a te il controllo.</p>"
-        ],
-        [
-            "question" => "<h2>Perché il mio account è associato a un paese?</h2>",
-            "answer" => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+            ],
+            [
+                "question" => "<h2>Perché il mio account è associato a un paese?</h2>",
+                "answer" => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
             <ol>
                 <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
                     <ul>
@@ -86,31 +106,30 @@
             <p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</p>
             <p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</p>
             <p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.</p>"
-        ],
-        [
-            "question" => "<h2>Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?</h2>",
-            "answer" => "<p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.</p>"
-        ],
-        [
-            "question" => "<h2>Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?</h2>",
-            "answer" => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>"
+            ],
+            [
+                "question" => "<h2>Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?</h2>",
+                "answer" => "<p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.</p>"
+            ],
+            [
+                "question" => "<h2>Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?</h2>",
+                "answer" => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>"
+            ]
+
         ]
 
-    ]
-
-    ?>
-    <div class="main_content" >
-        <?php 
-            foreach ($faqs as $faq){
+        ?>
+        <div class="main_content">
+            <?php
+            foreach ($faqs as $faq) {
                 // var_dump($faq);
                 echo ($faq["question"]);
                 echo ($faq["answer"]);
-                 
             }
-        
-        ?>
-    </div>
 
+            ?>
+        </div>
+    </main>
 
 
 
